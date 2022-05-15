edc7363fc7767f98ef53a10c649c5ad17243de5b
Derick Florencio <derick.florencio@bgmrodotec.com.br>
2022-05-15 13:08:58 -0300

    feat: add name, symbol, and balanceOf

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,15 @@ pragma solidity >= 0.4.22 < 0.9.0;
 
 contract DappToken {
 
-  uint256 public totalSupply;
+  string public name = 'Dapp Token';
+  string public symbol = 'DAPP';
+  string public standard = 'Dapp Token v1.0';
 
-  constructor() {
-    totalSupply = 1000000;
+  uint256 public totalSupply;
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) {
+    totalSupply = _initialSupply;
+    balanceOf[msg.sender] = totalSupply;
   }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,9 +1,27 @@
 var DappToken = artifacts.require('DappToken');
 
 contract('DappToken', accounts => {
+
+  it('initialized the toke with the correct values', async () => {
+    const instance = await DappToken.deployed();
+    const name = await instance.name();
+    assert.equal(name, 'Dapp Token');
+    const symbol = await instance.symbol();
+    assert.equal(symbol, 'DAPP');
+    const standard = await instance.standard();
+    assert.equal(standard, 'Dapp Token v1.0');
+  });
+
   it('sets the total supply upon deployment', async () => {
     const instance = await DappToken.deployed();
     const supply = await instance.totalSupply();
     assert.equal(supply, 1000000, 'sets the total supply to 1,000,000');
-  })
+  });
+
+  it('should mint all supply to the creator', async () => {
+    const instance = await DappToken.deployed();
+    const balance = await instance.balanceOf(accounts[0]);
+
+    assert.equal(balance, 1000000);
+  });
 });
